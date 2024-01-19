37daa2ecc2e23e8ea6055fd15480fdebed42ab5b
Zachary Ives <zives@hawk.iit.edu>
2024-01-19 13:39:07 -0600

    Added field for inputting radius and function to calculate outputs (unfinished)

--- SphereAndBoxCalculations(assignment1)/ContentView.swift
+++ SphereAndBoxCalculations(assignment1)/ContentView.swift
@@ -10,14 +10,27 @@ import SwiftUI
 struct ContentView: View {
     @Bindable var myBox = Box()
     @Bindable var mySphere = Sphere()
-    
+    @State var radiusString = "1.0"
     var body: some View {
-        HStack {
-            Text("Radius: ")
-            
-            //TextField("Enter radius of sphere ...")
+        VStack{
+            HStack {
+                Text("Radius: ")
+                    .padding(.top)
+                    .padding(.bottom, 15)
+                TextField("Enter radius of sphere ...", text:$radiusString, onCommit: {self.calculate()})
+                    .padding(.horizontal)
+                    .frame(width: 100)
+                    .padding(.top, 0)
+                    .padding(.bottom, 0)
+            }
         }
         .padding()
+    }
+    func calculate() {
+        mySphere.enableButton = false
+        myBox.enableButton = false
+        
+        let _ : Bool = mySphere.initWithRadius(r: Double(radiusString)!)
     }
 }
 
